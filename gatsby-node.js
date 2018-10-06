79170a80401712f3770a755c143bdbd6c45636e9
William Imoh <williamimoh@yahoo.com>
2018-10-06 20:59:20 +0100

    fixed unknown bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,40 +2,41 @@ const cloudinary = require('cloudinary')
 
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }, configOptions) => {
     const { createNode } = actions
+  
     delete configOptions.plugins
-
+  
     // Configure Cloudinary
     cloudinary.config({
         cloud_name: configOptions.cloudName,
         api_key: configOptions.apiKey,
         api_secret: configOptions.apiSecret
     })
-    
-  const processMedia = media => {
-      const nodeId = createNodeId(`cloudinary-media-${media.id}`)
-      const nodeContent = JSON.stringify(media)
-  
-      const nodeData = Object.assign({}, media, {
-      id: nodeId,
-      parent: null,
-      children: [],
-      internal: {
-          type: `CloudinaryMedia`,
-          content: nodeContent,
-          contentDigest: createContentDigest(media),
-      },
-      })
-  
-      return nodeData
-  }
 
-    return(
-        cloudinary.v2.api.resources({resource_type:'video'},(error, result)=>{
-        result.resources.forEach((mediaItem)=>{
-            const nodeData =processMedia(mediaItem)
-            createNode(nodeData)
+    const processMedia = media => {
+        const nodeId = createNodeId(`cloudinary-media-${media.public_id}`)
+        const nodeContent = JSON.stringify(media)
+
+        const nodeData = Object.assign({}, media, {
+        id: nodeId,
+        parent: null,
+        children: [],
+        internal: {
+            type: `CloudinaryMedia`,
+            content: nodeContent,
+            contentDigest: createContentDigest(media),
+        },
         })
-    })
-    )
 
+        return nodeData
+    }
+
+    return (
+        cloudinary.v2.api.resources({resource_type:'video'},(error, result)=>{
+            result.resources.forEach((mediaItem)=>{
+                const nodeData = processMedia(mediaItem)
+                createNode(nodeData)
+                console.log("Created Cloudinary file node")
+            })
+        })
+    )
   }
