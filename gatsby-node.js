91b39ce512f6b6a23bc31e7e250007624a5171ad
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-15 23:22:02 +0200

    added support for named transformations added computed thumb dims updated to latest cloudinary version

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,19 +12,42 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }, configOpt
         api_secret: configOptions.apiSecret
     })
 
-    const processMedia = media => {
+    /**
+     * @param {*} media 
+     * @param {Array} transformations 
+     */
+    function processMedia(media, transformations) {
         const nodeId = createNodeId(`cloudinary-media-${media.public_id}`)
+    
+        transformations.forEach(transformation => {
+            media[`${transformation}_image`] = {
+                url: cloudinary.url(media.public_id + '.jpg', {
+                    secure: false,
+                    transformation: [transformation]
+                }),
+                secure_url: cloudinary.url(media.public_id + '.jpg', {
+                    secure: true,
+                    transformation: [transformation]
+                })
+            }
+        })
+
+        media.thumb_dims = {
+            w: media.width < 640 ? media.width : 640,
+            h: media.width < 640 ? media.height : Math.floor((640 * media.height) / media.width)
+        }
+        
         const nodeContent = JSON.stringify(media)
 
         const nodeData = Object.assign({}, media, {
-        id: nodeId,
-        parent: null,
-        children: [],
-        internal: {
-            type: `CloudinaryMedia`,
-            content: nodeContent,
-            contentDigest: createContentDigest(media),
-        },
+            id: nodeId,
+            parent: null,
+            children: [],
+            internal: {
+                type: `CloudinaryMedia`,
+                content: nodeContent,
+                contentDigest: createContentDigest(media),
+            },
         })
 
         return nodeData
@@ -38,20 +61,23 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }, configOpt
     if(!!maxResults){queryParams.max_results = maxResults}
     if(!!type){queryParams.type = type}
     if(!!prefix && !!type){queryParams.prefix = prefix}
+    const transformations = configOptions.transformations || []
 
     return (
         cloudinary.v2.api.resources(queryParams,(error, result)=>{
             if(result.resources.length > 0){
-                result.resources.forEach((mediaItem)=>{
-                    const nodeData = processMedia(mediaItem)
+                result.resources.forEach( mediaItem => {
+                    const nodeData = processMedia(mediaItem, transformations)
                     createNode(nodeData)
-                    console.log("Created Cloudinary file node")
                 })  
-            }else{
+            } else {
                 console.log('\n ~Yikes! No Cloudinary files found and nodes not created. Try a better query.')
             }
-            if(error){console.log(error)}
             
+            if (error) {
+                console.log(error)
+            }
+            console.log(`Cloudinary rate limit remaining: ${result.rate_limit_remaining}`)
         })
     )
   }
