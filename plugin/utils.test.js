72c398b7373c895ddecd26952a5ce4ccfed9c194
raae <raae@raae.codes>
2023-03-10 16:13:46 +0100

    test: add test

--- plugin/utils.test.js
+++ plugin/utils.test.js
@@ -1,4 +1,11 @@
-import { getResourceOptions } from './utils';
+jest.mock('../package.json', () => ({
+  version: '0.1.6',
+}));
+jest.mock('gatsby/package.json', () => ({
+  version: '0.4.10',
+}));
+
+import { getResourceOptions, generateCloudinaryUrl } from './utils';
 
 describe('getResourceOptions', () => {
   it('transforms options to snake case', () => {
@@ -17,5 +24,42 @@ describe('getResourceOptions', () => {
     };
 
     expect(result).toEqual(expected);
+  });
+});
+
+describe('generateCloudinaryUrl', () => {
+  const ANALYTICS_CODE = 'AXOrEGQ0';
+
+  it('generates correct url for image (default)', () => {
+    const asset = {
+      public_id: 'public-id',
+      cloud_name: 'cloud-name',
+    };
+
+    const secureUrl = generateCloudinaryUrl(asset, { secure: true });
+    expect(secureUrl).toBe(
+      `https://res.cloudinary.com/cloud-name/image/upload/f_auto,q_auto/public-id?_a=${ANALYTICS_CODE}`,
+    );
+    const url = generateCloudinaryUrl(asset, { secure: false });
+    expect(url).toBe(
+      `http://res.cloudinary.com/cloud-name/image/upload/f_auto,q_auto/public-id?_a=${ANALYTICS_CODE}`,
+    );
+  });
+
+  it('generates correct url for video', () => {
+    const asset = {
+      public_id: 'public-id',
+      cloud_name: 'cloud-name',
+      resource_type: 'video',
+    };
+
+    const secureUrl = generateCloudinaryUrl(asset, { secure: true });
+    expect(secureUrl).toBe(
+      `https://res.cloudinary.com/cloud-name/video/upload/f_auto,q_auto/public-id?_a=${ANALYTICS_CODE}`,
+    );
+    const url = generateCloudinaryUrl(asset, { secure: false });
+    expect(url).toBe(
+      `http://res.cloudinary.com/cloud-name/video/upload/f_auto,q_auto/public-id?_a=${ANALYTICS_CODE}`,
+    );
   });
 });
